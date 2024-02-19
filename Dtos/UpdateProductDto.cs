3e92a47290b0e5fefd3c34deb3ab53523dae5cba
RatebBarakat <ratebbarakat2021@gmail.com>
2024-02-19 17:38:31 +0200

    add update product dto

---
+++ Dtos/UpdateProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using ecommerce.Models;
+
+namespace ecommerce.Dtos
+{
+    public class UpdateProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string SmallDescription { get; set; }
+        public string Description { get; set; }
+        public List<ProductImages> Images { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
