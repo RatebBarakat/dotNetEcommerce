3f4e475a44aaf39bf4960e47766993db38b65a3d
RatebBarakat <ratebbarakat2021@gmail.com>
2024-02-20 17:36:46 +0200

    add profile table

---
+++ Migrations/20240220152529_add_profile_table.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ecommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_profile_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Profiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Avatar = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Profiles_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_UserId",
+                table: "Profiles",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Profiles");
+        }
+    }
+}
---
+++ Models/Profile.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ecommerce.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+
+        public string Avatar { get; set; }
+
+        public string UserId { get; set; }
+        [JsonIgnore]
+        public virtual User User { get; set; }
+    }
+}
