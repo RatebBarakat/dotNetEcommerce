133287955ede2d06195b7d51bb816126f679ca2e
RatebBarakat <ratebbarakat2021@gmail.com>
2024-02-20 17:35:13 +0200

    improve validation logic

--- Validators/ImageValidator.cs
+++ Validators/ImageValidator.cs
@@ -6,7 +6,7 @@ namespace ecommerce.Validators
     {
         public ImageValidator()
         {
-            RuleFor(x => x.ContentType).NotNull().WithMessage("here is the error").Must(x => x.Equals("image/jpeg") || x.Equals("image/jpg") || x.Equals("image/png"))
+            RuleFor(x => x.ContentType).NotNull().WithMessage("image is required").Must(x => x.Equals("image/jpeg") || x.Equals("image/jpg") || x.Equals("image/png"))
                 .WithMessage("File type is not allowed");
         }
     }
--- Validators/ProductValidator.cs
+++ Validators/ProductValidator.cs
@@ -13,13 +13,14 @@ namespace ecommerce.Validators
         public ProductValidator(AppDbContext appDbContext)
         {
             _context = appDbContext;
-            RuleFor(p => p.Name).NotEmpty().MustAsync(UniqueName);
+            RuleFor(p => p.Name).NotEmpty().MustAsync(UniqueName).WithMessage("name should be unique");
             RuleFor(p => p.SmallDescription).MinimumLength(10).MaximumLength(100);
             RuleFor(p => p.Description).MinimumLength(100).MaximumLength(1000);
             RuleFor(p => p.Price).InclusiveBetween(0, int.MaxValue);
             RuleFor(p => p.Quantity).InclusiveBetween(0, 1000);
-            RuleFor(p => p.CategoryId).MustAsync(CategoryExists).WithMessage("category doeasnt exists");
-            RuleFor(p => p.Images).Must(x => x.Count > 0).WithMessage("ypu should upload at least one image")
+            RuleFor(p => p.CategoryId).MustAsync(CategoryExists).WithMessage("category doesn't exists");
+            RuleFor(p => p.Images)
+                .Must(x => x.Count > 0).WithMessage("ypu should upload at least one image")
                 .Must(x => x.Count <= 5).WithMessage("ypu cant upload more than 5 images");
             RuleForEach(p => p.Images).SetValidator(new ImageValidator());
         }
