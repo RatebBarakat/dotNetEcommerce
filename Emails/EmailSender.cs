0dec0a843e36ba29311a4f6b86cde838e2df3aa9
RatebBarakat <ratebbarakat2021@gmail.com>
2024-02-28 22:18:33 +0200

    make an abstract class for email send

---
+++ Emails/EmailSender.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+using MailKit.Net.Smtp;
+
+namespace ecommerce.Emails
+{
+    public abstract class EmailSender
+    {
+        public MimeMessage message = new MimeMessage();
+        public async Task<OkResult> SendEmailAsync(string to, string subject, TextPart html, string? from = null)
+        {
+            message.From.Add(MailboxAddress.Parse(from));
+            message.To.Add(MailboxAddress.Parse(to));
+            message.Subject = subject;
+            message.Body = html;
+
+            using var smtp = new SmtpClient();
+            await smtp.ConnectAsync("smtp.ethereal.email", 587, false);
+            await smtp.AuthenticateAsync("cary.kemmer@ethereal.email", "p143ejA52XKnU7kT2t");
+            await smtp.SendAsync(message);
+            await smtp.DisconnectAsync(true);
+
+            return new OkResult();
+        }
+    }
+}
--- Emails/SendEmailVerificationLink.cs
+++ Emails/SendEmailVerificationLink.cs
@@ -1,4 +1,5 @@
-﻿using ecommerce.Models;
+﻿using ecommerce.Interfaces;
+using ecommerce.Models;
 using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -9,22 +10,11 @@ using System.Threading.Tasks;
 
 namespace ecommerce.Emails
 {
-    public class SendEmailVerificationLink
+    public class SendEmailVerificationLink : EmailSender
     {
-        private readonly IHttpContextAccessor _httpContextAccessor;
-
-        public SendEmailVerificationLink(IHttpContextAccessor httpContextAccessor)
-        {
-            _httpContextAccessor = httpContextAccessor;
-        }
 
         public async Task<IActionResult> SendAsync(string email, string url)
         {
-            var emailContent = new MimeMessage();
-            emailContent.From.Add(MailboxAddress.Parse("cyrus.weissnat@ethereal.email"));
-            emailContent.To.Add(MailboxAddress.Parse(email));
-            emailContent.Subject = "test";
-
             var templatesFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "Templates");
             var filePath = Path.Combine(templatesFolderPath, "EmailConfirmation.html");
 
@@ -33,13 +23,9 @@ namespace ecommerce.Emails
                 var htmlContent = await stream.ReadToEndAsync();
                 htmlContent = string.Format(htmlContent, url, "Please confirm your email");
 
-                emailContent.Body = new TextPart(TextFormat.Html) { Text = htmlContent };
+                var html = new TextPart(TextFormat.Html) { Text = htmlContent };
 
-                using var smtp = new SmtpClient();
-                await smtp.ConnectAsync("smtp.ethereal.email", 587, false);
-                await smtp.AuthenticateAsync("cyrus.weissnat@ethereal.email", "NyCF2ndsHB18ZQpBxz");
-                await smtp.SendAsync(emailContent);
-                await smtp.DisconnectAsync(true);
+                await base.SendEmailAsync(email, "email confirm", html);
 
                 return new OkResult();
             }
