c97d092aac253ee0d3dd5f7b9613aa5ba7e39346
RatebBarakat <ratebbarakat2021@gmail.com>
2024-02-29 13:28:53 +0200

    separate dtos and specify when either user is admin or not

---
+++ Dtos/UserWithPermissionsDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ecommerce.Dtos
+{
+    public class UserWithPermissionsDTO
+    {
+        public UserDTO User { get; set; }
+
+        public bool IsAdmin { get; set; }
+        public ICollection<string> Permissions { get; set; } = new List<string>();
+    }
+}
